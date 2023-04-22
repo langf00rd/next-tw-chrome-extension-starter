85a1c65b64b972845af3b68fd4ae8eb7c484f025
Langford Kwabena <langfordquarshie21@gmail.com>
2023-04-22 10:08:42 +0000

    🚀

---
+++ next-app/pages/index.js
@@ -0,0 +1,15 @@
+const IndexPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <h1 className="text-4xl text-center font-medium leading-[50px] p-5">
+        Build a Chrome Extension with
+        <br />
+        <span className="text-blue-800">Next.js</span> + <span className="text-blue-600">Tailwind CSS</span>
+        <br />
+        🚀
+      </h1>
+    </div>
+  );
+};
+
+export default IndexPage;
